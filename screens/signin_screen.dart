9f481025f7e15fb9d8363e5d3bca2828fc88615e
kurushiba <156669290+kurushiba@users.noreply.github.com>
2024-07-15 10:09:46 +0900

    Update signin_screen.dart

--- screens/signin_screen.dart
+++ screens/signin_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sns_app/widgets/form_text_field.dart';
-import 'package:sns_app/widgets/keyboard_aware.dart';
 
 class SigninScreen extends StatefulWidget {
   const SigninScreen({super.key});
@@ -20,78 +19,74 @@ class SigninScreenState extends State<SigninScreen> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: KeyboardAware(
-        child: Column(
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Container(
-                  padding: const EdgeInsets.all(16.0),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Sign In',
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    FormTextField(
+                      label: 'Email',
+                      hintText: 'Enter your email',
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                    const SizedBox(height: 16.0),
+                    FormTextField(
+                      label: 'Password',
+                      hintText: 'Enter your password',
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 16.0),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor:
+                              WidgetStateProperty.resolveWith<Color>((states) {
+                            if (states.contains(WidgetState.disabled)) {
+                              return Colors.grey;
+                            }
+                            return Colors.black;
+                          }),
+                        ),
+                        child: const Text(
+                          'Sign In',
+                          style: TextStyle(color: Colors.white),
                         ),
                       ),
-                      const SizedBox(height: 16.0),
-                      FormTextField(
-                        label: 'Email',
-                        hintText: 'Enter your email',
-                        keyboardType: TextInputType.emailAddress,
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.blue,
+                        textStyle: const TextStyle(
+                            decoration: TextDecoration.underline),
                       ),
-                      const SizedBox(height: 16.0),
-                      FormTextField(
-                        label: 'Password',
-                        hintText: 'Enter your password',
-                        obscureText: true,
-                      ),
-                      const SizedBox(height: 16.0),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ButtonStyle(
-                            backgroundColor:
-                                WidgetStateProperty.resolveWith<Color>(
-                                    (states) {
-                              if (states.contains(WidgetState.disabled)) {
-                                return Colors.grey;
-                              }
-                              return Colors.black;
-                            }),
-                          ),
-                          child: const Text(
-                            'Sign In',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                      TextButton(
-                        style: TextButton.styleFrom(
-                          foregroundColor: Colors.blue,
-                          textStyle: const TextStyle(
-                              decoration: TextDecoration.underline),
-                        ),
-                        onPressed: () =>
-                            Navigator.pushNamed(context, '/signup'),
-                        child: const Text("Sign Up"),
-                      )
-                    ],
-                  ),
+                      onPressed: () => Navigator.pushNamed(context, '/signup'),
+                      child: const Text("Sign Up"),
+                    )
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
